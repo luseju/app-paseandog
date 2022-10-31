dc2250ef3a15ea0ed40b4cfe2d6848f04bd9c262
julianaeg <julianaecheverrig@gmail.com>
2022-10-31 08:49:03 -0500

    registro

--- app/src/main/java/com/example/app_paseandog/registro.kt
+++ app/src/main/java/com/example/app_paseandog/registro.kt
@@ -3,11 +3,17 @@ package com.example.app_paseandog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.app_paseandog.databinding.ActivityRegistroBinding
 
 class registro : AppCompatActivity() {
+    private lateinit var binding:ActivityRegistroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
+
+        binding.btnInicio.setOnClickListener{
+            startActivity(Intent(this,MainActivity::class.java))
+        }
 
     }
 }
