d575ae905c7f73451df8db4d790ed0d736fce0f3
julianaeg <julianaecheverrig@gmail.com>
2022-10-30 21:42:20 -0500

    splash

--- app/src/main/java/com/example/app_paseandog/MainActivity.kt
+++ app/src/main/java/com/example/app_paseandog/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_Apppaseandog)
+        Thread.sleep(3000)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
